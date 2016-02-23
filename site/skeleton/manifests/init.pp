3b4784d0d91eb5c50226edcc37f5b4a61be5ce35
Steve <stepkirk@users.noreply.github.com>
2016-02-23 11:03:54 -0800

    Forgot quotes around source

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -6,7 +6,7 @@ class skeleton {
   
   file { "/etc/skel/.bashrc":
     ensure => file,
-    source => puppet:///modules/skeleton/bashrc,
+    source => 'puppet:///modules/skeleton/bashrc',
   }
   
 }
