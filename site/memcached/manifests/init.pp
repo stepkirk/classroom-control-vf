80dbe63cadbbb0f579ec07ddfc4e1d5364375b60
Steve <stepkirk@users.noreply.github.com>
2016-02-23 14:07:55 -0800

    Add class wrapper

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,18 +1,22 @@
-package { 'memcached':
-  ensure => present,
-}
+class memcached {
 
-file { '/etc/sysconfig/memcached':
-  ensure => file,
-  owner => 'root',
-  group => 'root',
-  mode => '0644',
-  source => 'puppet:///modules/memcached/memcached',
-  require => Package['memcached'],
-}
+  package { 'memcached':
+    ensure => present,
+  }
 
-service { 'memcached':
-  ensure => running,
-  enable => true,
-  subscribe => File['/etc/sysconfig/memcached'],
+  file { '/etc/sysconfig/memcached':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    source => 'puppet:///modules/memcached/memcached',
+    require => Package['memcached'],
+  }
+
+  service { 'memcached':
+    ensure => running,
+    enable => true,
+    subscribe => File['/etc/sysconfig/memcached'],
+  }
+
 }
