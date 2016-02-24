30de948f30e56dcaf6eb12da1b05351efce66850
Steve <stepkirk@users.noreply.github.com>
2016-02-24 09:42:43 -0800

    Refactor code to set defaults and remove redundancies

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,39 +1,33 @@
 class nginx {
 
+  File {
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+  }
+  
   package { 'nginx':
     ensure => present,
   }
   
   file { '/var/www':
     ensure => directory,
-    owner => 'root',
-    group => 'root',
-    mode => '0755',
   }
 
   file { '/var/www/index.html':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
     source => 'puppet:///modules/nginx/index.html',
     require => Package['nginx'],
   }
   
   file { '/etc/nginx/nginx.conf':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
     source => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
   }
   
   file { '/etc/nginx/conf.d/default.conf':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
     source => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
   }
