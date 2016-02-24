7bb64a74998516810abbd38bcaf326b6f43a96c7
Steve <stepkirk@users.noreply.github.com>
2016-02-24 14:20:09 -0800

    Add admin users

---
+++ site/users/manifests/admins.pp
@@ -0,0 +1,12 @@
+class users::admins {
+  users::managed_user { 'jose': }
+  users::managed_user { 'alice':
+    group => 'staff',
+  }
+  users::managed_user { 'chen':
+    group => 'staff',
+  }
+  group { 'staff':
+    ensure => present,
+  }
+}
