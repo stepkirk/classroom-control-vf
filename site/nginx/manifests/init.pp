237eb5eccc1802b30636ba6bbd1be055ecf355ef
Steve <stepkirk@users.noreply.github.com>
2016-02-23 22:06:30 -0800

    Missing closing bracket

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -34,6 +34,6 @@ class nginx {
   service { 'nginx':
     ensure => running,
     enable => true,
-    subscribe => [File['/etc/nginx/nginx.conf'], File['/etc/nginx/conf.d/default.conf'],
+    subscribe => [File['/etc/nginx/nginx.conf'], File['/etc/nginx/conf.d/default.conf']],
   }
 }
