f7f9815f23fac11265b43318b569052ee1bed77b
Steve <stepkirk@users.noreply.github.com>
2016-02-24 14:14:48 -0800

    Add support for .ssh directory

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -9,4 +9,10 @@ define users::managed_user (
       owner => $title,
       group => $group,
     }
+    file { "/home/$title/.ssh":
+      ensure => directory,
+      owner => $title,
+      group => $group,
+      mode => "0700",
+    }
   }
